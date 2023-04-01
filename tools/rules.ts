51c8a98e49ce025aeca85271174df145a0b9b418
grscwf <grayscale+ghtm@gmail.com>
2023-03-31 22:52:16 -0700

    preparing to rename passage draft tags

--- tools/rules.ts
+++ tools/rules.ts
@@ -8,7 +8,7 @@ const common = [
   "tw-common",
   // Note: fast-glob has a bug with "tw-drekkar-?"
   "tw-drekkar-*",
-]
+];
 
 // Note: pathnames are relative to cwd
 export const rules: Rule[] = [
@@ -19,6 +19,6 @@ export const rules: Rule[] = [
   {
     target: "nero.html",
     dirs: [...common, "tw-nero-*", "tw-nero-0"],
-    omit: ["tw-nero-wip"],
+    // omit: ["tw-nero-3", "tw-nero-wip"],
   },
 ];
