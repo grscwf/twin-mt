2ea54e104584d1a5f9049e0c3c2b531c407c76a4
grscwf <grayscale+ghtm@gmail.com>
2024-09-09 07:35:24 -0700

    manually specify version string

--- tools/make-version.ts
+++ tools/make-version.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env ts-node
 /*
- * ts-node make-version.ts [--deploy]
+ * ts-node make-version.ts $version [--deploy]
  *
  * Create a new version and tag it.
  */
@@ -12,43 +12,64 @@ import fsP from "fs/promises";
 const top = `${__dirname}/..`;
 const versionJS = `${top}/tw-code/c0-Game-Version.js`;
 
+function fatal(message: string, ...args: unknown[]): never {
+  if (args.length) {
+    console.error(message, ...args);
+  }
+  throw new Error(message);
+}
+
+function nonfatal(
+  force: boolean | undefined,
+  message: string,
+  ...args: unknown[]
+) {
+  if (force) {
+    console.error(message, ...args);
+  } else {
+    fatal(message, ...args);
+  }
+}
+
 async function main(argv: string[]) {
   const program = new Command()
     .option("--deploy", "deploy the new version to mt2")
     .option("--push", "push to origin")
-    .parse(argv);
+    .option("--force", "ignore error checks");
+  program.argument("<version>");
+  program.parse(argv);
   const opts = program.opts();
 
   const status = await runP("git status --porcelain", { echo: true });
   if (status !== "") {
-    throw new Error(`checkout is not clean`);
+    nonfatal(opts.force, "checkout is not clean");
   }
 
   const text = await fsP.readFile(versionJS, "utf-8");
   const m = /(setup\.version = ")(.+)"/.exec(text);
   if (m == null) {
-    throw new Error(`Couldn't find version in ${versionJS}`);
+    fatal(`couldn't find version in ${versionJS}`);
   }
 
   const oldVersion = m[2]!;
   if (!/-nero-wip$/.test(oldVersion)) {
-    throw new Error(`Expected version to be -nero-wip, not ${oldVersion}`);
+    nonfatal(opts.force, `Expected version to be -nero-wip, not ${oldVersion}`);
   }
 
-  const time = new Date().toISOString()
-    .replace(/\..*/, "")
-    .replace(/[-:]/g, "")
-    .replace(/T/, "-");
+  const version = program.args[0]!;
 
-  const version = `v2.0-beta-${time}`;
+  const versionRE = /^v2[.]0-beta-\d+$/;
+  if (!versionRE.test(version)) {
+    nonfatal(opts.force, `Version ${version} does not match ${versionRE}`);
+  }
 
   const newText =
-    text.slice(0, m.index + m[1]!.length)
-    + version
-    + text.slice(m.index + m[0].length - 1);
+    text.slice(0, m.index + m[1]!.length) +
+    version +
+    text.slice(m.index + m[0].length - 1);
 
   await fsP.writeFile(versionJS, newText);
-  await runP(`ts-node ${top}/tools/tw-to-html.ts`)
+  await runP(`ts-node ${top}/tools/tw-to-html.ts`);
 
   await runP(`git commit -am "${version}"`, { echo: true });
   await runP(`git tag "${version}"`, { echo: true });
@@ -58,13 +79,13 @@ async function main(argv: string[]) {
   }
 
   await fsP.writeFile(versionJS, text);
-  await runP(`ts-node ${top}/tools/tw-to-html.ts`)
+  await runP(`ts-node ${top}/tools/tw-to-html.ts`);
   await runP(`git commit -am "Revert to ${oldVersion}"`, { echo: true });
   if (opts.push) {
     await runP(`git push origin HEAD "${version}" 2>&1`, { echo: true });
   }
 }
 
-main(process.argv).catch(e => {
+main(process.argv).catch((e) => {
   throw e;
 });
